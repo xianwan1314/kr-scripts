3e5d5fdc73d8cbe1992d5ed52b5d0e0d4c7ab8b7
zjadzhd <116424564+zjadzhd@users.noreply.github.com>
2024-03-09 22:15:51 +0800

    update Some Code

--- pio/src/main/java/com/projectkr/shell/DialogPower.kt
+++ pio/src/main/java/com/projectkr/shell/DialogPower.kt
@@ -9,30 +9,15 @@ class DialogPower(var context: Activity) {
     fun showPowerMenu() {
         val view = context.layoutInflater.inflate(R.layout.dialog_power_operation, null)
         val dialog = DialogHelper.customDialog(context, view)
-        view.findViewById<View>(R.id.power_shutdown).setOnClickListener {
+        fun onclick(cmd:String){
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_shutdown_cmd))
+            KeepShellPublic.doCmdSync(cmd)
         }
-        view.findViewById<View>(R.id.power_reboot).setOnClickListener {
-            dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_reboot_cmd))
-        }
-        view.findViewById<View>(R.id.power_hot_reboot).setOnClickListener {
-            dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_hot_reboot_cmd))
-        }
-
-        view.findViewById<View>(R.id.power_recovery).setOnClickListener {
-            dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_recovery_cmd))
-        }
-        view.findViewById<View>(R.id.power_fastboot).setOnClickListener {
-            dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_fastboot_cmd))
-        }
-        view.findViewById<View>(R.id.power_emergency).setOnClickListener {
-            dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_emergency_cmd))
-        }
+        view.findViewById<View>(R.id.power_shutdown).setOnClickListener { onclick(context.getString(R.string.power_shutdown_cmd)) }
+        view.findViewById<View>(R.id.power_reboot).setOnClickListener { onclick(context.getString(R.string.power_reboot_cmd)) }
+        view.findViewById<View>(R.id.power_hot_reboot).setOnClickListener { onclick(context.getString(R.string.power_hot_reboot_cmd)) }
+        view.findViewById<View>(R.id.power_recovery).setOnClickListener { onclick(context.getString(R.string.power_recovery_cmd)) }
+        view.findViewById<View>(R.id.power_fastboot).setOnClickListener { onclick(context.getString(R.string.power_fastboot_cmd)) }
+        view.findViewById<View>(R.id.power_emergency).setOnClickListener { onclick(context.getString(R.string.power_emergency_cmd)) }
     }
 }
